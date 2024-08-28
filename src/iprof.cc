334b39bc9b77bc4c8fb2f238d642703772581d65
Noah Goldstein <goldstein.w.n@gmail.com>
2024-08-28 13:59:13 -0700

    Steamer

--- src/iprof.cc
+++ src/iprof.cc
@@ -42,12 +42,48 @@ extern "C" {
 #define IPROF_ENV_GET_LLVM_MATTRS "IPROF_LLVM_MATTRS"
 
 #define iprof_assert(expr) assert(expr)
+#define iprof_unreachable(msg)                                                 \
+  fprintf(stderr, "%s\n", msg);                                                \
+  std::abort()
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
 namespace iprof {
 
 namespace llvm_ext {
+class StreamingSourceMgr : public llvm::mca::SourceMgr {
+  std::deque<UniqueInst> Staging;
+  /// Current instruction index.
+  unsigned TotalCounter = 0U;
+
+  /// End-of-stream flag.
+  bool EOS = false;
+
+public:
+  StreamingSourceMgr() = default;
+  bool hasNext() const override { return !Staging.empty(); }
+  bool isEnd() const override { return EOS; }
+  llvm::ArrayRef<UniqueInst> getInstructions() const override {
+    iprof_unreachable("Invalid in this context");
+  }
+  llvm::mca::SourceRef peekNext() const override {
+    iprof_assert(hasNext());
+    return llvm::mca::SourceRef(TotalCounter, *Staging.front());
+  }
+
+  /// Add a new instruction.
+  void addInst(UniqueInst &&Inst) { Staging.push_back(std::move(Inst)); }
+  void updateNext() override {
+    iprof_assert(hasNext());
+    ++TotalCounter;
+    Staging.front().release();
+    Staging.pop_front();
+  }
+
+  /// Mark the end of instruction stream.
+  void endOfStream() { EOS = true; }
+};
+
 /// A view that collects and prints a few performance numbers.
 class SummaryView : public llvm::mca::View {
   const llvm::MCSchedModel &SM;
@@ -155,111 +191,6 @@ public:
   }
 };
 } // namespace llvm_ext
-
-namespace detail::wyhash {
-
-static inline void mum(uint64_t *a, uint64_t *b) {
-#if defined(__SIZEOF_INT128__)
-  __uint128_t r = *a;
-  r *= *b;
-  *a = static_cast<uint64_t>(r);
-  *b = static_cast<uint64_t>(r >> 64U);
-#elif defined(_MSC_VER) && defined(_M_X64)
-  *a = _umul128(*a, *b, b);
-#else
-  uint64_t ha = *a >> 32U;
-  uint64_t hb = *b >> 32U;
-  uint64_t la = static_cast<uint32_t>(*a);
-  uint64_t lb = static_cast<uint32_t>(*b);
-  uint64_t hi{};
-  uint64_t lo{};
-  uint64_t rh = ha * hb;
-  uint64_t rm0 = ha * lb;
-  uint64_t rm1 = hb * la;
-  uint64_t rl = la * lb;
-  uint64_t t = rl + (rm0 << 32U);
-  auto c = static_cast<uint64_t>(t < rl);
-  lo = t + (rm1 << 32U);
-  c += static_cast<uint64_t>(lo < t);
-  hi = rh + (rm0 >> 32U) + (rm1 >> 32U) + c;
-  *a = lo;
-  *b = hi;
-#endif
-}
-
-// multiply and xor mix function, aka MUM
-static inline uint64_t mix(uint64_t a, uint64_t b) {
-  mum(&a, &b);
-  return a ^ b;
-}
-
-// read functions. WARNING: we don't care about endianness, so results are
-// different on big endian!
-static inline uint64_t r8(const uint8_t *p) {
-  uint64_t v{};
-  std::memcpy(&v, p, 8U);
-  return v;
-}
-
-static inline uint64_t r4(const uint8_t *p) {
-  uint32_t v{};
-  std::memcpy(&v, p, 4);
-  return v;
-}
-
-// reads 1, 2, or 3 bytes
-static inline uint64_t r3(const uint8_t *p, size_t k) {
-  return (static_cast<uint64_t>(p[0]) << 16U) |
-         (static_cast<uint64_t>(p[k >> 1U]) << 8U) | p[k - 1];
-}
-
-static inline uint64_t hash(void const *key, size_t len) {
-  static constexpr uint64_t secret[4] = {
-      UINT64_C(0xa0761d6478bd642f), UINT64_C(0xe7037ed1a0b428db),
-      UINT64_C(0x8ebc6af09c88c6e3), UINT64_C(0x589965cc75374cc3)};
-
-  auto const *p = static_cast<uint8_t const *>(key);
-  uint64_t seed = secret[0];
-  uint64_t a{};
-  uint64_t b{};
-  if (len <= 16) {
-    if (len >= 4) {
-      a = (r4(p) << 32U) | r4(p + ((len >> 3U) << 2U));
-      b = (r4(p + len - 4) << 32U) | r4(p + len - 4 - ((len >> 3U) << 2U));
-    } else if (len > 0) {
-      a = r3(p, len);
-      b = 0;
-    } else {
-      a = 0;
-      b = 0;
-    }
-  } else {
-    size_t i = len;
-    if (i > 48) {
-      uint64_t see1 = seed;
-      uint64_t see2 = seed;
-      do {
-        seed = mix(r8(p) ^ secret[1], r8(p + 8) ^ seed);
-        see1 = mix(r8(p + 16) ^ secret[2], r8(p + 24) ^ see1);
-        see2 = mix(r8(p + 32) ^ secret[3], r8(p + 40) ^ see2);
-        p += 48;
-        i -= 48;
-      } while (i > 48);
-      seed ^= see1 ^ see2;
-    }
-    while (i > 16) {
-      seed = mix(r8(p) ^ secret[1], r8(p + 8) ^ seed);
-      i -= 16;
-      p += 16;
-    }
-    a = r8(p + i - 16);
-    b = r8(p + i - 8);
-  }
-
-  return mix(secret[1] ^ len, mix(a ^ secret[1], b ^ seed));
-}
-
-} // namespace detail::wyhash
 
 static const char *gArchName = NULL;
 static constexpr size_t kMaxInstLength = 16;
@@ -541,6 +472,53 @@ static void IProfOnExit(qemu_plugin_id_t id, void *userdata) {
                                /*CallLatency=*/3);
 
     llvm::mca::IncrementalSourceMgr SrcMgr;
+    llvm::mca::PipelineOptions PO(/*MicroOpQueue=*/0, /*DecoderThroughput=*/0,
+                                  /*DispatchWidth=*/0,
+                                  /*RegisterFileSize=*/0,
+                                  /*LoadQueueSize=*/0, /*StoreQueueSize=*/0,
+                                  /*AssumeNoAlias=*/true,
+                                  /*EnableBottleneckAnalysis=*/false);
+
+    std::unique_ptr<llvm::mca::CustomBehaviour> CB =
+        std::make_unique<llvm::mca::CustomBehaviour>(*STI, SrcMgr, *MCII);
+    llvm::mca::Context MCtx(*MRI, *STI);
+    auto P = MCtx.createDefaultPipeline(PO, SrcMgr, *CB);
+
+    std::unique_ptr<llvm_ext::SummaryView> SV =
+        std::make_unique<llvm_ext::SummaryView>(STI->getSchedModel(), Insts,
+                                                PO.DispatchWidth);
+    P->addEventListener(SV.get());
+
+    for (const llvm::MCInst &I : Insts) {
+      llvm::Expected<std::unique_ptr<llvm::mca::Instruction>> Inst =
+          IB.createInstruction(I, Instruments);
+      assert(Inst);
+      SrcMgr.addInst(std::move(Inst.get()));
+      llvm::Expected<unsigned> Cycles = P->run();
+      assert(Cycles || Cycles.errorIsA<llvm::mca::InstStreamPause>());
+    }
+
+    SrcMgr.endOfStream();
+    llvm::Expected<unsigned> Cycles = P->run();
+
+    assert(Cycles);
+    llvm::json::Value Result = SV->toJSON();
+    std::string ResStr;
+    llvm::raw_string_ostream ROS(ResStr);
+    ROS << Result;
+    fprintf(fp, "%s\n", ResStr.c_str());
+
+    fclose(fp);
+  }
+
+  {
+    FILE *fp = fopen("result3.json", "w+");
+    std::unique_ptr<llvm::mca::InstrumentManager> IM =
+        std::make_unique<llvm::mca::InstrumentManager>(*STI, *MCII);
+    llvm::mca::InstrBuilder IB(*STI, *MCII, *MRI, MCIA.get(), *IM,
+                               /*CallLatency=*/3);
+
+    llvm_ext::StreamingSourceMgr SrcMgr;
     llvm::mca::PipelineOptions PO(/*MicroOpQueue=*/0, /*DecoderThroughput=*/0,
                                   /*DispatchWidth=*/0,
                                   /*RegisterFileSize=*/0,
